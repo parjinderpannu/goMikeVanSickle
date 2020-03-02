120621a5d5ca95595cf41b8ff9e38ade344f15b6
PARJINDER S PANNU <parjinderpannu@gmail.com>
2020-03-02 14:58:50 -0800

    constant at package level

--- main.go
+++ main.go
@@ -2,42 +2,45 @@ package main
 
 import "fmt"
 
+const pi = 3.1415
+
 func main() {
-	const pi = 3.1415 // declare & initialize at same line
 	fmt.Println(pi)
+	// const pi = 3.1415 // declare & initialize at same line
+	// fmt.Println(pi)
 
-	const c int = 3
-	fmt.Println(c + 3)
+	// const c int = 3
+	// fmt.Println(c + 3)
 
-	// a bunch of code
-	fmt.Println(float32(c) + 1.2)
+	// // a bunch of code
+	// fmt.Println(float32(c) + 1.2)
 
-	firstName := "Arthur"
-	fmt.Println(firstName)
+	// firstName := "Arthur"
+	// fmt.Println(firstName)
 
-	ptr := &firstName
-	fmt.Println(ptr, *ptr)
+	// ptr := &firstName
+	// fmt.Println(ptr, *ptr)
 
-	firstName = "Tricia"
-	fmt.Println(ptr, *ptr)
+	// firstName = "Tricia"
+	// fmt.Println(ptr, *ptr)
 
 	// var firstName *string = new(string)
 	// *firstName = "Arthur"
 	// fmt.Println(*firstName)
 
 	// fmt.Println("Hello, from a module, Gophers")
-	var i int // initialize variable
-	i = 42    // give value
-	fmt.Println(i)
+	// var i int // initialize variable
+	// i = 42    // give value
+	// fmt.Println(i)
 
-	var f float32 = 3.14
-	fmt.Println(f)
+	// var f float32 = 3.14
+	// fmt.Println(f)
 
 	// firstName := "Arthur"
 	// fmt.Println(firstName)
 
-	b := true
-	fmt.Println(b)
+	// b := true
+	// fmt.Println(b)
 
 	// c := complex(3, 4)
 	// fmt.Println(c)
