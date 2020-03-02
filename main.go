d83cbea47108d51f1c86ff8a30db96c9454353ae
PARJINDER S PANNU <parjinderpannu@gmail.com>
2020-03-02 15:14:46 -0800

    long initialization

--- main.go
+++ main.go
@@ -2,18 +2,26 @@ package main
 
 import "fmt"
 
-const (
-	first = iota
-	second
-)
+// const (
+// 	first = iota
+// 	second
+// )
 
-const (
-	third = iota
-	fourth
-)
+// const (
+// 	third = iota
+// 	fourth
+// )
 
 func main() {
-	fmt.Println(first, second, third, fourth)
+	var arr [3]int // easier for compiler
+	arr[0] = 1
+	arr[1] = 2
+	arr[2] = 3
+	fmt.Println(arr)
+	fmt.Println(arr[1])
+	// fmt.Println(arr[4]) // invalid array index 4 (out of bounds for 3-element array)
+
+	// fmt.Println(first, second, third, fourth)
 	// const pi = 3.1415 // declare & initialize at same line
 	// fmt.Println(pi)
 
