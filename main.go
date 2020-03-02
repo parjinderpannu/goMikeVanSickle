eae24bb108bbe45c9c90e84db264015b8a37ab18
PARJINDER S PANNU <parjinderpannu@gmail.com>
2020-03-02 15:18:21 -0800

    1st example of slice

--- main.go
+++ main.go
@@ -13,16 +13,22 @@ import "fmt"
 // )
 
 func main() {
-	var arr [3]int // easier for compiler
-	arr[0] = 1
-	arr[1] = 2
-	arr[2] = 3
-	fmt.Println(arr)
-	fmt.Println(arr[1])
-	// fmt.Println(arr[4]) // invalid array index 4 (out of bounds for 3-element array)
+	arr := [3]int{1, 2, 3}
 
-	arr2 := [3]int{1, 2, 3}
-	fmt.Println(arr2)
+	slice := arr[:]
+
+	fmt.Println(arr, slice)
+
+	// var arr [3]int // easier for compiler
+	// arr[0] = 1
+	// arr[1] = 2
+	// arr[2] = 3
+	// fmt.Println(arr)
+	// fmt.Println(arr[1])
+	// // fmt.Println(arr[4]) // invalid array index 4 (out of bounds for 3-element array)
+
+	// arr2 := [3]int{1, 2, 3}
+	// fmt.Println(arr2)
 
 	// fmt.Println(first, second, third, fourth)
 	// const pi = 3.1415 // declare & initialize at same line
