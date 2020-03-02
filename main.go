4cbbfb529528955445ccc92ac8f150a12e055f4a
PARJINDER S PANNU <parjinderpannu@gmail.com>
2020-03-01 17:37:20 -0800

    more about pointers

--- main.go
+++ main.go
@@ -4,9 +4,18 @@ import "fmt"
 
 func main() {
 
-	var firstName *string = new(string)
-	*firstName = "Arthur"
-	fmt.Println(*firstName)
+	firstName := "Arthur"
+	fmt.Println(firstName)
+
+	ptr := &firstName
+	fmt.Println(ptr, *ptr)
+
+	firstName = "Tricia"
+	fmt.Println(ptr, *ptr)
+
+	// var firstName *string = new(string)
+	// *firstName = "Arthur"
+	// fmt.Println(*firstName)
 
 	// fmt.Println("Hello, from a module, Gophers")
 	var i int // initialize variable
