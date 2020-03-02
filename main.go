54d73c756992024b3f15b1f2f3d572818d643962
PARJINDER S PANNU <parjinderpannu@gmail.com>
2020-03-01 17:22:27 -0800

    different ways to initialize and assign value

--- main.go
+++ main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, from a module, Gophers")
+	// fmt.Println("Hello, from a module, Gophers")
+	var i int // initialize variable
+	i = 42    // give value
+	fmt.Println(i)
+
+	var f float32 = 3.14
+	fmt.Println(f)
+
+	firstName := "Arthur"
+	fmt.Println(firstName)
 }
