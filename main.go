ec7aa0763ad9669f16607ab853f5cdc902d2c9eb
PARJINDER S PANNU <parjinderpannu@gmail.com>
2020-03-02 17:13:34 -0800

    create func

--- main.go
+++ main.go
@@ -6,4 +6,11 @@ import (
 
 func main() {
 	fmt.Println("Hello, playground")
+	startWebServer()
+}
+
+func startWebServer() {
+	fmt.Println("Starting server ...")
+	// do imp stuff
+	fmt.Println("Server started")
 }
