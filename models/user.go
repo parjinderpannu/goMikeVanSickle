f3d30fb118fbcac0a3dcd4487e59958c4c901fba
PARJINDER S PANNU <parjinderpannu@gmail.com>
2020-03-02 17:37:59 -0800

    get and add user func

--- models/user.go
+++ models/user.go
@@ -10,3 +10,14 @@ var (
 	users  []*User // slice of pointers of user
 	nextID = 1
 )
+
+func GetUsers() []*User {
+	return users
+}
+
+func AddUser(u User) (User, error) {
+	u.ID = nextID
+	nextID++
+	users = append(users, &u)
+	return u, nil
+}
