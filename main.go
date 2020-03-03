bb6e098a7c967fedada996049ded26696e4d24ba
PARJINDER S PANNU <parjinderpannu@gmail.com>
2020-03-02 17:25:32 -0800

    func return

--- main.go
+++ main.go
@@ -7,12 +7,13 @@ import (
 func main() {
 	fmt.Println("Hello, playground")
 	port := 3000
-	startWebServer(port, 2)
+	isStarted := startWebServer(port)
+	fmt.Println(isStarted)
 }
 
-func startWebServer(port, numberOfRetries int) {
+func startWebServer(port int) bool {
 	fmt.Println("Starting server ...")
 	// do imp stuff
 	fmt.Println("Server started on port", port)
-	fmt.Println("Number of retries", numberOfRetries)
+	return true
 }
